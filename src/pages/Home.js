e92de417da3020c4275bd5fda523ad0e0c8b1ae6
nazakatmammadova <b066042021@asoiu.edu.az>
2023-04-11 12:27:39 +0400

    fix bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import '../App.css'
+import Cards from '../components/Cards'
+import Footer from '../components/Footer'
 
 function Home (){
     return(
     <>
         <HeroSection/>
+        <Cards/>
+        <Footer/>
     </>
     )
 }
